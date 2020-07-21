85975977594836a9e808096453154bea281c7259
Beka <beka.haile@miltolabs.com>
2020-07-21 13:49:45 +0300

    changed image display

--- src/components/news_components/file_text_viewer.vue
+++ src/components/news_components/file_text_viewer.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="content">
     <div>
-        <figure class="pr-lg-3  figureClass">
+        <figure class="mr-lg-4 pt-2 figureClass">
             <img alt="Image" class="centerImage d-block m-auto" :src="dataPassed.url">
-            <figcaption>
-               <i style="opacity:0.7;" >{{dataPassed.caption}}</i>
+            <figcaption >
+               <i style="opacity:0.7;">{{dataPassed.caption}}</i>
              </figcaption>
         </figure>
         <TextViewer :dataPassed="dataPassed"  class="newsText"/>
